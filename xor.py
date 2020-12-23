37bddead5970bf15d381970fdac854d08c0c0a46
jczic <jczic.bos@gmail.com>
2020-12-23 10:22:16 +0100

    add XOR example

---
+++ xor.py
@@ -0,0 +1,24 @@
+﻿
+# -*- coding: utf-8 -*-
+
+from microMLP import MicroMLP
+
+mlp = MicroMLP.Create( neuronsByLayers           = [2, 2, 1],
+                       activationFuncName        = MicroMLP.ACTFUNC_GAUSSIAN,
+                       layersAutoConnectFunction = MicroMLP.LayersFullConnect )
+
+nnFalse  = MicroMLP.NNValue.FromBool(False)
+nnTrue   = MicroMLP.NNValue.FromBool(True)
+
+mlp.AddExample( [nnFalse, nnFalse], [nnFalse] )
+mlp.AddExample( [nnFalse, nnTrue ], [nnTrue ] )
+mlp.AddExample( [nnTrue , nnTrue ], [nnFalse] )
+mlp.AddExample( [nnTrue , nnFalse], [nnTrue ] )
+
+learnCount = mlp.LearnExamples()
+
+print( "LEARNED :" )
+print( "  - False xor False = %s" % mlp.Predict([nnFalse, nnFalse])[0].AsBool )
+print( "  - False xor True  = %s" % mlp.Predict([nnFalse, nnTrue] )[0].AsBool )
+print( "  - True  xor True  = %s" % mlp.Predict([nnTrue , nnTrue] )[0].AsBool )
+print( "  - True  xor False = %s" % mlp.Predict([nnTrue , nnFalse])[0].AsBool )
